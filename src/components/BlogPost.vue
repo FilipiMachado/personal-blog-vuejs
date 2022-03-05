4a1ac22fb958b1ca46d5da0fd4b826c1f676413d
filipi <filipisee@hotmail.com>
2022-03-05 18:01:56 -0300

    blogpost finished

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="blog-wrapper">
+  <div class="blog-wrapper no-user">
     <div class="blog-content">
       <div>
         <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
@@ -9,7 +9,7 @@
         <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
           Login / Cadastrar-se<Arrow class="arrow arrow-light" />
         </router-link>
-        <router-link class="link link-light" v-else to="#">
+        <router-link class="link" v-else to="#">
           Veja o Post<Arrow class="arrow arrow-light" />
         </router-link>
       </div>
@@ -38,14 +38,20 @@ export default {
 
 <style scoped>
 .link {
-  cursor: pointer;
-  text-decoration: none;
-  text-transform: uppercase;
-  color: black;
+  display: inline-flex;
+  align-items: center;
+  margin-top: 32px;
+  padding-bottom: 4px;
+  border-bottom: 1px solid transparent;
+  transition: .5s ease-in all;
 }
 
-.link-light {
-  color: #fff;
+.link:hover {
+  border-bottom-color: #303030;
+}
+
+.link-light:hover {
+  border-bottom-color: #fff;
 }
 
 .blog-wrapper {
@@ -63,6 +69,72 @@ export default {
   order: 2;
 }
 
+.blog-photo {
+  order: 1;
+  flex: 3;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+
+.blog-photo img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.blog-content div {
+  max-width: 375px;
+  padding: 72px 24px;
+}
+
+.blog-content h2 {
+  font-size: 32px;
+  font-weight: 300;
+  text-transform: uppercase;
+  margin-bottom: 24px;
+}
+
+.blog-content p {
+  font-size: 15px;
+  font-weight: 300;
+  line-height: 1.7;
+}
+
+.content-preview {
+  font-size: 13px;
+  max-height: 24px;
+  width: 250px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.blog-wrapper:nth-child(even) .blog-content{
+  order: 2;
+}
+
+.blog-wrapper:nth-child(even) .blog-photo{
+  order: 1;
+}
+
+.arrow {
+  margin-left: 8px;
+  width: 12px;
+}
+
+.arrow path {
+  fill: #000;
+}
+
+.arrow-light path {
+  fill: #000;
+}
+
+.no-user:first-child .blog-content{
+  background-color: #303030;
+  color: #fff;
+}
+
 @media (min-width: 700px) {
 .blog-wrapper {
   min-height: 650px;
@@ -73,11 +145,27 @@ export default {
 .blog-content {
   order: 1;
 }
+
+.blog-content div {
+  padding: 0 24px;
+}
+
+.blog-content h2 {
+  font-size: 40px;
+}
+
+.blog-photo {
+  order: 2;
+}
 }
 
 @media (min-width: 800px) {
 .blog-content {
   flex: 3;
+}
+
+.blog-photo {
+  flex: 4;
 }
 }
 </style>
