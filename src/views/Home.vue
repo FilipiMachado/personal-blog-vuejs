9c36d5c243c2a6508a20a09ee936953a27eb748f
filipi <filipisee@hotmail.com>
2022-03-05 16:36:51 -0300

    adding structure to Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+
   </div>
 </template>
 
@@ -8,7 +8,29 @@
 
 export default {
   name: 'Home',
-  components: {
+  components: {},
+  data() {
+    return {
+      welcomeScreen: {
+        title: 'Welcome',
+        blogPost:
+          'Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post',
+        welcomeScreen: true,
+        photo: 'coding'
+      },
+      sampleBlogPost: [
+        {
+          title: 'This is a filler Title!',
+          blogHTML: 'This is a filler blog post title!',
+          blogCoverPhoto: 'beautiful-stories'
+        },
+        {
+          title: 'This is a filler Title!',
+          blogHTML: 'This is a filler blog post title!',
+          blogCoverPhoto: 'beautiful-stories'
+        },
+      ]
+    }
   }
 }
 </script>
