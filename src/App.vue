0211180a35ef770a0546bd40136d0d877d3adf93
filipi <filipisee@hotmail.com>
2022-03-10 16:28:40 -0300

    finished state update

--- src/App.vue
+++ src/App.vue
@@ -27,8 +27,14 @@ export default {
     }
   },
   created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.$store.commit('updateUser', user)
+      if (user) {
+        this.$store.dispatch('getCurrentUser')
+        console.log(this.$store.state.profileEmail)
+      }
+    })
     this.checkRoute()
-    console.log(firebase.auth().currentUser)
   },
   mounted() {},
   methods: {
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import db from "../firebase/firebaseInit"
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -23,9 +27,31 @@ export default new Vuex.Store({
   mutations: {
     toggleEditPost(state, payload) {
       state.editPost = payload
-    }
+    },
+    updateUser(state, payload) {
+      state.user = payload
+    },
+    setProfileInfo(state, doc) {
+      state.profileId = doc.id
+      state.profileEmail = doc.data().email
+      state.profileFirstName = doc.data().firstName
+      state.profileLastName = doc.data().lastName
+      state.profileUsername = doc.data().username
+    },
+    setProfileInitials(state) {
+      state.profileInitials =
+        state.profileFirstName.match(/(\b\S)?/g).join("") + 
+        state.profileLastName.match(/(\b\S)?/g).join("");
+    },
   },
   actions: {
+    async getCurrentUser({commit}) {
+      const dataBase = await db.collection('users').doc(firebase.auth().currentUser.uid)
+      const dbResults = await dataBase.get()
+      commit('setProfileInfo', dbResults)
+      commit('setProfileInitials')
+      console.log(dbResults)
+    }
   },
   modules: {
   }
