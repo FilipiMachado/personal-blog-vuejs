15b3d20d734db0260c55a66144a859b1d2baa438
filipi <filipisee@hotmail.com>
2022-03-09 23:20:44 -0300

    finished loading component

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>Loading</div>
+  <div class="loading">
+    <span></span>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'Loading',
   data() {
     return {};
   },
@@ -11,4 +14,33 @@ export default {
 </script>
 
 <style scoped>
+.loading {
+  top: 0;
+  z-index: 101;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@keyframes spin {
+  to {
+    transform: rotateZ(360deg);
+  }
+}
+
+.loading span {
+  display: block;
+  width: 60px;
+  height: 60px;
+  margin-top: 0 auto;
+  border: 3px solid transparent;
+  border-top-color: #fff;
+  border-bottom-color: #fff;
+  border-radius: 50%;
+  animation: spin ease 1000ms infinite;
+}
 </style>
